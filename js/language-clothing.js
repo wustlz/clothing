9de3ff419fcce411674d92a7a3b81a9730066313
frank <wustfrank@126.com>
2020-04-07 22:49:17 +0800

    完成name界面

---
+++ js/language-clothing.js
@@ -0,0 +1,37 @@
+
+var lan = "en";
+
+$.ready(function () {
+    // alert("页面加载时调用的方法");
+    lan = $.i18n.normaliseLanguageCode({}); //获取浏览器的语言
+    if (lan !== 'zh') {
+        lan = 'en';
+    }
+    loadProperties(lan);
+});
+
+function changeLanguage() {
+    lan_text = $('.data-i18n-lan').text();
+    if (lan_text === '切换中文') {
+        lan = 'zh';
+    } else {
+        lan = 'en';
+    }
+    loadProperties(lan);
+}
+
+function loadProperties(type) {
+    $.i18n.properties({
+        name: 'clothing', // 资源文件名称
+        language: type, // 对应的语言
+        path: '/clothing/i18n/resource/', // 资源文件所在目录路径
+        mode: 'map', // 模式：变量或 Map
+        cache: false,
+        encoding: 'UTF-8',
+        callback: function () { // 回调方法
+            $.each($.i18n.map, function (key, value) {
+                $('.'+key).html($.i18n.prop(key));
+            });
+        }
+    });
+}
--- js/language-name.js
+++ js/language-name.js
@@ -1,11 +1,13 @@
 
 var lan = "en";
+var datetimepicker_lan = 'en';
 
-$.ready(function () {
+$(function () {
     // alert("页面加载时调用的方法");
     lan = $.i18n.normaliseLanguageCode({}); //获取浏览器的语言
     if (lan !== 'zh') {
         lan = 'en';
+        datetimepicker_lan='en';
     }
     loadProperties(lan);
 });
@@ -14,17 +16,26 @@ function changeLanguage() {
     lan_text = $('.data-i18n-lan').text();
     if (lan_text === '切换中文') {
         lan = 'zh';
+        datetimepicker_lan='zh-cn';
     } else {
         lan = 'en';
+        datetimepicker_lan='en';
     }
-    loadProperties(lan);
+    loadProperties(lan, datetimepicker_lan);
 }
 
-function loadProperties(type) {
+function loadProperties(type, date_lan) {
+    // $('#name-birthday').html('<input type="text" class="form-control" /><span class="input-group-addon">' +
+    //     '<span class="glyphicon glyphicon-calendar"></span></span>');
+    $('#name-birthday').datetimepicker({
+        format: 'YYYY-MM-DD',
+        locale: moment.locale(date_lan)
+    });
+    $('#name-birthday')
     $.i18n.properties({
-        name: 'common', // 资源文件名称
+        name: 'clothing', // 资源文件名称
         language: type, // 对应的语言
-        path: '/i18n/resource/', // 资源文件所在目录路径
+        path: '/clothing/i18n/resource/', // 资源文件所在目录路径
         mode: 'map', // 模式：变量或 Map
         cache: false,
         encoding: 'UTF-8',
