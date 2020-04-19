3860ada3292ef24726c3c5256653c11720b267bc
liaojinwei <ljw>
2020-04-19 22:05:55 +0800

    1、添加分享按钮；2、添加name-生肖

--- js/custom-name.js
+++ js/custom-name.js
@@ -1,6 +1,6 @@
 function reCss() {
     var nameHeight = $('.detail-name').height();
-    for (var idx = 1; idx <= 6; idx++) {
+    for (var idx = 0; idx <= 6; idx++) {
         var masterElement = $('#name-master-height-' + idx.toString());
         var slaveElement = $('#name-slave-height-' + idx.toString());
         var height = masterElement.height();
@@ -12,6 +12,7 @@ function reCss() {
         slaveElement.css("height", height);
         slaveElement.children('p').css("margin-top", (height - slaveElement.children('p').height()) / 2 + 5);
     }
+
 }
 
 $(function () {
@@ -21,3 +22,15 @@ $(function () {
 $(window).resize(function () {
     reCss();
 });
+
+
+function share() {
+    $('#share-1').share({sites: ['qzone', 'qq', 'weibo','wechat']});
+    $('#share-0').hidden();
+    $('#share-1').show();
+}
+
+function unshare() {
+    $('#share-0').hidden();
+    $('#share-1').show();
+}
